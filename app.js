702bbf2888322930b9b8a7d91a116d4fa1ec160a
Avery <avery.cantrell@gmail.com>
2021-08-09 01:28:51 -0400

    Added some comments

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const settings = require('./settings/appSettings');
 const routes = require('./lib/routes');
 
+// attempt to connect to locally hosted database
 mongoose.connect(settings.dbURL, { useNewUrlParser: true, useFindAndModify: false }).
     catch(error => {
         console.log(`ERROR: cannot connect to database, check URL! \n ${error}`);
